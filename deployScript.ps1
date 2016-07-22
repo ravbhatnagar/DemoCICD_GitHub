9c98426a1e13ac129fb7989d127445c3896b7961
Gaurav Bhatnagar <ravbhatnagar@hotmail.com>
2016-07-22 16:22:20 -0700

    Update deployScript.ps1

--- deployScript.ps1
+++ deployScript.ps1
@@ -4,6 +4,6 @@ Param(
     [string] [Parameter(Mandatory=$true)] $TemplateParameterUri
     )
     
-Get-Module
+Get-Module -ListAvailable
 Get-Module -Name AzureRM.Resources
 Test-AzureRmResourceGroupDeployment -ResourceGroupName temprg5 -TemplateUri $TemplateUri -TemplateParameterUri $TemplateParameterUri -Debug -Force
